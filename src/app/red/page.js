8bcb9bca4869f9401ba5c3ce07e70716c11525f1
adnanysf <asubyusuf@gmail.com>
2024-06-08 13:23:58 -0500

    Initial commit

---
+++ src/app/red/page.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+
+export default function Red(){
+    return(
+        <div>
+            <h1>Red</h1>
+            <Link href="/">Press Me to GO back</Link>
+        </div>
+    )
+}
